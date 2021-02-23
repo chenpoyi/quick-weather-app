e4d03d1815939a90e148d1aa8156d723b864f79b
Paul Chen <paulchen647@gmail.com>
2021-02-23 14:04:49 -0800

    Add search bar withou autocomplete functionality.'

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,16 +1,29 @@
-// import PlacesAutocomplete from 'react-places-autocomplete';
-
+import React from 'react';
+import './Search.scss';
+// import {getLocations} from '../helpers/selectors';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+// import GooglePlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-google-places-autocomplete';
+// require('dotenv').config();
 
 // const Search = function(props){
 
-//   return (
-//     <>
-//     </>
+//   const searchProps = {
+//     apiKey: process.env.REACT_APP_MAPS_API,
+//     minLengthAutocomplete: 2
+//   }
+//   geocodeByAddress('Montevideo, Uruguay')
+//   .then(results => console.log(results));
+
+//   return(
+//     <div id="search-container">
+//           <GooglePlacesAutocomplete id={'search-bar'} {...searchProps}/>
+
+//     </div>
 //   )
-// };
+// }
 
-
-import React from 'react';
+// export default Search;
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng,
@@ -22,7 +35,6 @@ class Search extends React.Component {
     this.state = { address: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
-
   }
  
   handleChange(address){
@@ -38,10 +50,44 @@ class Search extends React.Component {
  
   render() {
     return (
-      <>
-      </>
+      <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+               
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
     );
   }
 }
+
 
 export default Search;
---
+++ client/src/helpers/selectors.js
@@ -0,0 +1,5 @@
+// const cities = require('all-the-cities');
+// export function getLocations(){
+//   // return cities.filter(city => city.name.match('Albuquerque'));
+
+// }
